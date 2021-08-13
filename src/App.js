fe95e44e354df8dbdaf0de0be5a98d8b8fd921ae
Ilona Kedracka <ilona.kedracka@gmail.com>
2021-08-13 12:34:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and test GH notifications vol2.
+          Edit <code>src/App.js</code> and test GH notifications vol3 działaj no.
         </p>
         <a
           className="App-link"
